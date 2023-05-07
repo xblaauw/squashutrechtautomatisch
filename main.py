88ca8d97cefe1f3a637d08039404fb25ed760421
harrie <harriedekat@gmail.com>
2023-05-07 16:34:30 +0200

    2x per week automatisch
    ingesteld om 2x per week 7 dagen vooruit om 19:00 te reserveren

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 username = 'xblaauw'
 password = 'Tying-Breach-Hazing8'
-
+DEV = True
+print('devmode', DEV)
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
@@ -8,16 +9,22 @@ from selenium.webdriver.common.by import By
 import datetime as dt
 import pandas as pd
 from time import sleep
+from random import randint
+
+wait_min = 25
+wait_random_sec = randint(0, wait_min*60)
+print('waiting', pd.Timedelta(wait_random_sec, unit='s'))
+if not DEV: sleep(wait_random_sec)
 
 
 # pick a date based on what day it is today
-today = dt.date.today()
+today = dt.date.today() + dt.timedelta(days=7)
 desired_date = today
-desired_time = dt.time(18, 45)
+desired_time = dt.time(19, 00)
 
 # Set up the browser driver
 driver_options = Options()
-driver_options.add_argument("--headless")
+if not DEV: driver_options.add_argument("--headless")
 driver = webdriver.Firefox(options=driver_options)
 
 # Navigate to the login page
@@ -55,7 +62,6 @@ availability = availability.rename_axis(['timeslots', 'lanes'])
 # select which lane and time if there are any:
 availability_wide = availability['available'].unstack('lanes')
 availability_wide = availability_wide.drop('76', axis=1)
-print(availability_wide.to_string())
 
 def parse_availability_times(times):
     return times.to_series().apply(lambda x: pd.to_datetime(x[:5]).time())
@@ -63,34 +69,47 @@ def parse_availability_times(times):
 start_times = parse_availability_times(availability_wide.index)
 available_at_desired_time = availability_wide.loc[start_times == desired_time].squeeze().dropna()
 
-try:
-    # select the back lanes first if multiple are available
-    chosen_lane = available_at_desired_time.sort_index(ascending=False).head(1).index[0]
+print('available at', desired_time)
+available_at_desired_time_print = available_at_desired_time
+available_at_desired_time_print.index = available_at_desired_time_print.index.astype(int) - 121
+print(available_at_desired_time_print)
+# select the back lanes first if multiple are available
+lane_rank = pd.Series({
+    '127': 0,
+    '126': 1,
+    '128': 2,
+    '125': 3,
+    '129': 4,
+    '124': 5,
+    '130': 6,
+    '123': 7
+}).sort_values()
 
-    table_index = availability.xs(chosen_lane, level='lanes')
-    table_index = table_index.loc[parse_availability_times(table_index.index)==desired_time, 'table_index']
-    
-    print(
-        '\ndesired_date: ', desired_date, 
-        '\ndesired_time: ', desired_time, 
-        '\nchosen_lane: ', chosen_lane, 
-        '\ntable_index: ', table_index.squeeze()
-    )
+for lane, rank in lane_rank.items():
+    if available_at_desired_time.index.isin([lane]).any():
+        chosen_lane = lane
+        break
 
-    # click the selected item in the table
-    table[table_index.squeeze()].click()
+# chosen_lane = available_at_desired_time.sort_index(ascending=False).head(1).index[0]
 
-    # we land with the cursor in the "Speler 2" veld
-    sleep(3)
-    driver.find_element(by=By.CLASS_NAME, value='ms-search').send_keys('Jaron Heising')
-    sleep(3)
-    driver.find_element(by=By.ID, value='__make_submit').click()
-    sleep(3)
-    driver.find_element(by=By.ID, value='__make_submit2').click()
-    
-    print('SUCCESS!')
-except Exception as e:
-    driver.quit()
-    raise e
+table_index = availability.xs(chosen_lane, level='lanes')
+table_index = table_index.loc[parse_availability_times(table_index.index)==desired_time, 'table_index']
 
+# click the selected item in the table
+table[table_index.squeeze()].click()
 
+# we land with the cursor in the "Speler 2" veld
+sleep(3)
+driver.find_element(by=By.CLASS_NAME, value='ms-search').send_keys('Jaron Heising')
+sleep(3)
+driver.find_element(by=By.ID, value='__make_submit').click()
+sleep(3)
+driver.find_element(by=By.ID, value='__make_submit2').click()
+
+print('\n\n')
+print(dt.datetime.now().replace(microsecond=0), 'SUCCESS!')
+print(
+    '\ndesired_date: ', desired_date, 
+    '\ndesired_time: ', desired_time, 
+    '\nchosen_lane:  ', int(chosen_lane)-121
+)
